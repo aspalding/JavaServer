2aef2b686f7b730a69fa129f1620583a2207e958
aspalding <andrewtspalding@gmail.com>
2014-08-11 16:07:32 -0500

    fixed simultaneous

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,20 +1,25 @@
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
+import java.util.Stack;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class Application{
     public static void main (String cat[]) throws Exception{
         ArgsParser.parseArguments(Arrays.asList(cat));
 
         System.setProperty("user.dir", ArgsParser.root);
-        ServerSocket s = new ServerSocket(ArgsParser.port);
+        ServerSocket s = new ServerSocket(ArgsParser.port, 4096);
 
-        ExecutorService exe = Executors.newFixedThreadPool(8);
+        ExecutorService exe = Executors.newFixedThreadPool(32);
+        BlockingQueue<Socket> socketQueue = new LinkedBlockingDeque<>();
 
         while(!s.isClosed()){
-            Thread t = new Thread(new ServerWorker(s.accept()));
-            exe.submit(t);
+            socketQueue.put(s.accept());
+            exe.submit(new ServerWorker(socketQueue.take()));
         }
     }
 }

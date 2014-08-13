b8d488b2fb2f85e2901087af59e8425167e549ee
aspalding <andrewtspalding@gmail.com>
2014-08-13 15:44:35 -0500

    completes auth

--- src/main/java/Responses/LogsRoute.java
+++ src/main/java/Responses/LogsRoute.java
@@ -3,13 +3,16 @@ package Responses;
 import Requests.Request;
 import Responses.Persistence.Logs;
 
+import java.util.Base64;
 import java.util.HashMap;
 
 public class LogsRoute implements Route {
     Request request;
+    String validUser;
 
     public LogsRoute(Request request){
         this.request = request;
+        validUser = "admin:hunter2";
     }
 
     public Response respond(){
@@ -22,8 +25,19 @@ public class LogsRoute implements Route {
     public Response get(){
         if(!request.headers.containsKey("Authorization"))
             return new Response(401, "Unauthorized", new HashMap<>(), "Authentication required".getBytes());
+        else if(!isValidUser(decodeString(request.headers.get("Authorization"))))
+            return new Response(401, "Unauthorized", new HashMap<>(), "Authentication required".getBytes());
         else
             return new Response(200, "OK", new HashMap<>(), generateBody());
+    }
+
+    public String decodeString(String user){
+        user = user.substring(6);
+        return new String(Base64.getDecoder().decode(user));
+    }
+
+    public boolean isValidUser(String user){
+        return user.equals(validUser);
     }
 
     public byte[] generateBody(){
--- src/test/java/Responses/LogsRouteTest.java
+++ src/test/java/Responses/LogsRouteTest.java
@@ -9,8 +9,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LogsRouteTest {
-    Request authorizedRequest, unauthorizedRequest, notAllowedRequest;
-    LogsRoute authorizedRoute, unauthorizedRoute, notAllowedRoute;
+    Request authorizedRequest, invalidRequest, unauthorizedRequest, notAllowedRequest;
+    LogsRoute authorizedRoute, invalidRoute, unauthorizedRoute, notAllowedRoute;
 
     @Before
     public void setUp() throws Exception {
@@ -21,6 +21,14 @@ public class LogsRouteTest {
                         "Host: localhost:5000"
         );
         authorizedRoute = new LogsRoute(authorizedRequest);
+
+        invalidRequest = new Request(
+                "GET /logs HTTP/1.1\n" +
+                        "Authorization: Basic YWRtaW46cGFzc3dvcmQ=\n" +
+                        "Connection: close\n" +
+                        "Host: localhost:5000"
+        );
+        invalidRoute = new LogsRoute(invalidRequest);
 
         unauthorizedRequest = new Request(
                 "GET /logs HTTP/1.1\n" +
@@ -49,6 +57,8 @@ public class LogsRouteTest {
     public void testRespond() throws Exception {
         assert notAllowedRoute.respond().status == 405;
         assert authorizedRoute.respond().status == 200;
+        assert invalidRoute.respond().status == 401;
+        assert unauthorizedRoute.respond().status == 401;
     }
 
     @Test
@@ -61,5 +71,15 @@ public class LogsRouteTest {
     public void testGenerateBody() throws Exception {
         Router.route(unauthorizedRequest);
         assert authorizedRoute.generateBody().length > 0;
+    }
+
+    @Test
+    public void testDecodeString() throws Exception{
+        assertEquals("admin:hunter2", authorizedRoute.decodeString("Basic YWRtaW46aHVudGVyMg=="));
+    }
+
+    @Test
+    public void testIsValidUser() throws Exception{
+        assert authorizedRoute.isValidUser("admin:hunter2");
     }
 }
